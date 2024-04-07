8f8190915fd2667cbabbfdbde9665163f203a8f2
Diamantis Skapinakis <diamantis.skapinakis@channelvas.com>
2024-04-07 22:14:40 +0300

    streamlit app setting

---
+++ app_frontend.py
@@ -0,0 +1,31 @@
+
+import streamlit as st
+import requests
+
+def main():
+
+    st.title('Concrete Crack Classification App')
+
+    col1, col2, col3 = st.columns([1, 5, 1])  
+
+    with col1:
+        st.write("") 
+
+    with col2:
+        st.write("")  
+        st.image(r'C:/Users/dskapinakis/Documents/computer_vision_concrete/crack_images/Positive/00030.jpg', width=300, caption="", use_column_width=True)
+        uploaded_file = st.file_uploader("Upload a concrete image")
+        if uploaded_file:
+            if st.button('Probability of concrete having a crack'):
+                files = {'file': uploaded_file}
+                response = requests.post("http://127.0.0.1:8000/prediction/", files=files)
+                response.raise_for_status()
+                prediction = response.json() 
+                st.write("Probability of concrete being cracked:", prediction['prediction'])
+
+    with col3:
+        st.write("") 
+
+if __name__ == "__main__":
+    main()
+
