0f4559320bf50c5bd7b8f24e7c88c07bdca3a358
Diamantis Skapinakis <diamantis.skapinakis@channelvas.com>
2024-04-07 14:34:19 +0300

    add exceptions

--- main.py
+++ main.py
@@ -26,36 +26,34 @@ def root_route():
 @app.post('/prediction/', response_model=Prediction)
 async def prediction_route(file: UploadFile = File(...)):
 
-  # Ensure that this is an image
-  if file.content_type.startswith('image/') is False:
-    raise HTTPException(status_code=400, detail=f'File \'{file.filename}\' is not an image.')
+    # Ensure that the input is an image
+    if file.content_type.startswith('image/') is False:
+        raise HTTPException(status_code=400, detail=f'File \'{file.filename}\' is not an image.')
+    
+    try:
+        # Read image contents
+        contents = await file.read()
+        pil_image = Image.open(io.BytesIO(contents))
 
-  try:
-    # Read image contents
-    contents = await file.read()
-    pil_image = Image.open(io.BytesIO(contents))
+        # Resize image to expected input shape
+        pil_image = pil_image.resize((input_shape[1], input_shape[2]))
 
-    # Resize image to expected input shape
-    pil_image = pil_image.resize((input_shape[1], input_shape[2]))
+        # Convert from RGBA to RGB *to avoid alpha channels*
+        if pil_image.mode == 'RGBA':
+            pil_image = pil_image.convert('RGB')
 
-    # Convert from RGBA to RGB *to avoid alpha channels*
-    if pil_image.mode == 'RGBA':
-      pil_image = pil_image.convert('RGB')
+        # Convert image into numpy format
+        numpy_image = np.expand_dims(pil_image, axis=0)
 
-    # Convert image into numpy format
-    numpy_image = np.array(pil_image).reshape((input_shape[1], input_shape[2], input_shape[3]))
+        # Generate prediction
+        predictions = model.predict(numpy_image)
+        prediction = predictions[0][0]
 
-    # Generate prediction
-    predictions = model.predict(numpy_image)
-    prediction = predictions[0][0]
-
-    return {
-      'filename': file.filename,
-      'contenttype': file.content_type,
-      'prediction': prediction.tolist(),
-    }
-  except:
-    e = sys.exc_info()[1]
-    raise HTTPException(status_code=500, detail=str(e))
-  
-
+        return {
+        'filename': file.filename,
+        'contenttype': file.content_type,
+        'prediction': prediction.tolist(),
+        }
+    except:
+        e = sys.exc_info()[1]
+        raise HTTPException(status_code=500, detail=str(e))
